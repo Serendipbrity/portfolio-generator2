921127abbdb1109f51102e3ecb8e7fde936f19e9
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-04-21 17:42:49 -0400

    question promt

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+const promptUser = () => {
 
-console.log(profileDataArgs);
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ]);
 
-const [name, github] = profileDataArgs;
+};
+    promptUser().then(answers => console.log(answers));
 
-console.log(name, github);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
