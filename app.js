214cf9f0a728aca80721a5eb7c2eb61c607b8f4b
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-04-23 14:08:17 -0400

    add another project

--- app.js
+++ app.js
@@ -7,11 +7,82 @@ const promptUser = () => {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+           type: 'input',
+           name: 'github',
+           message: 'Enter your Github Username' 
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
         }
     ]);
-
 };
-    promptUser().then(answers => console.log(answers));
+
+const promptProject = portfolioData => {
+    console.log(`
+    ==================
+    Add a New Project 
+    ==================
+`);
+// if theres no projects array property, create one
+    if (!portfolioData.projects){
+    portfolioData.projects =[];
+    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['Javascript','HTML','CSS','ES6','jQuery','Bootstrap','Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the Github link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        // if user wants to add more projects, then the condition evaluates to true and calls promptProject(portfolioData) function
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+            // if user decides not to add more projects then the condition will evaluate to false and trigger portfolioData so the object is returned
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => 
+        {console.log(portfolioData)
+        });
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
